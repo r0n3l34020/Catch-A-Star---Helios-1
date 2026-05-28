fb5a11e1158345775a7f3cd7efa85861d6c5d761
r0n3l34020 <ronel.jonathan30@gmail.com>
2026-05-28 15:43:25 +0400

    Update plot_sunspot_graph.py

--- visualization/plot_sunspot_graph.py
+++ visualization/plot_sunspot_graph.py
@@ -2,15 +2,12 @@ import sunpy.map
 import matplotlib.pyplot as plt
 import os
 
-# Get the path to one of your files
 folder = "sunspot_images_hmi"
-file_name = os.listdir(folder)[0]  # Grab the first file in the folder
+file_name = os.listdir(folder)[0]
 file_path = os.path.join(folder, file_name)
 
-# Create a SunPy Map object
 hmi_map = sunpy.map.Map(file_path)
 
-# Plot the image
 plt.figure(figsize=(8, 8))
 hmi_map.plot()
 plt.colorbar(label="Intensity")
